4455a7f4231b46179898e829139e6a03528e8ade
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2025-05-23 21:03:18 -0300

    main/command: Rename XRayCommand to ToggleXRayCommand

--- src/main/java/santoro/serverXRay/ServerXRay.java
+++ src/main/java/santoro/serverXRay/ServerXRay.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import santoro.serverXRay.command.ToggleXRayCommand;
 import santoro.serverXRay.xray.XRayRenderer;
 
 public class ServerXRay extends JavaPlugin {
@@ -19,7 +20,7 @@ public class ServerXRay extends JavaPlugin {
 
         glowing = new GlowingBlocks(this);
         XRayRenderer.init(glowing);
-        getCommand("xray").setExecutor(this);
+        getCommand("xray").setExecutor(new ToggleXRayCommand());
     }
 
     @Override
--- src/main/java/santoro/serverXRay/command/ToggleXRayCommand.java
+++ src/main/java/santoro/serverXRay/command/ToggleXRayCommand.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class XRayCommand implements CommandExecutor {
+public class ToggleXRayCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
